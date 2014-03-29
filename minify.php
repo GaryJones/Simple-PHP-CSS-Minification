eb3db7cb26c636d53ec3e2cd9f39ebe04cfc6e5a
Caio Santana Magalhães <caiosm1005@users.noreply.github.com>
2014-03-29 00:24:53 -0300

    Update minify.php

--- minify.php
+++ minify.php
@@ -20,11 +20,11 @@ function minify( $css ) {
 	// Remove ; before }
 	$css = preg_replace( '/;(?=\s*})/', '', $css );
 
-	// Remove space after , : ; { }
-	$css = preg_replace( '/(,|:|;|\{|}) /', '$1', $css );
+	// Remove space after , : ; { } */
+	$css = preg_replace( '/(,|:|;|\{|}|\*\/) /', '$1', $css );
 
-	// Remove space before , ; { }
-	$css = preg_replace( '/ (,|;|\{|})/', '$1', $css );
+	// Remove space before , ; { } ( ) >
+	$css = preg_replace( '/ (,|;|\{|}|\(|\)|>)/', '$1', $css );
 
 	// Strips leading 0 on decimal values (converts 0.5px into .5px)
 	$css = preg_replace( '/(:| )0\.([0-9]+)(%|em|ex|px|in|cm|mm|pt|pc)/i', '${1}.${2}${3}', $css );
